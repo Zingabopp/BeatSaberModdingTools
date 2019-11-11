71ce3b991a9a9c3d168d810664c19b1d55cc22e0
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-11-11 02:12:01 -0600

    Started work on storing BeatSaberDir inside VS

--- Beat Saber Mod Templates/Beat_Saber_Mod_TemplatesPackage.cs
+++ Beat Saber Mod Templates/Beat_Saber_Mod_TemplatesPackage.cs
@@ -25,6 +25,7 @@ namespace BeatSaberModTemplates
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(BeatSaberModTemplatesPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class BeatSaberModTemplatesPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace BeatSaberModTemplates
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await SetBeatSaberDirCommand.InitializeAsync(this);
         }
 
         #endregion
