a209c06f13b2fe22dba08894781e452c82109be5
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-11-18 11:57:21 -0600

    UI pretty much finished.

---
+++ Beat Saber Mod Templates/Models/SettingsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatSaberModTemplates.Models
+{
+    public class SettingsModel
+    {
+        public BeatSaberInstall ChosenInstall { get; set; }
+
+        public bool GenerateUserFileWithTemplate { get; set; }
+
+        public bool GenerateUserFileOnExisting { get; set; }
+
+        public bool SetManifestJsonDefaults { get; set; }
+
+
+    }
+}
