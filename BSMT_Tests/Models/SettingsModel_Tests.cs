1edebdc70cbe7e691e01a192d0968ff79d9defbe
Zingabopp <zingabopp@gmail.com>
2019-11-20 18:03:57 -0600

    Settings model tests

--- BSMT_Tests/Models/SettingsModel_Tests.cs
+++ BSMT_Tests/Models/SettingsModel_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using BeatSaberModTemplates.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,8 +8,22 @@ namespace BSMT_Tests.Models
     [TestClass]
     public class SettingsModel_Tests
     {
+        private static SettingsModel AllChanged => new SettingsModel()
+        {
+            ChosenInstallPath = @"C:\Test\Testing",
+            GenerateUserFileWithTemplate = true,
+            GenerateUserFileOnExisting = true,
+            SetManifestJsonDefaults = true,
+            CopyToIPAPendingOnBuild = true,
+            BuildReferenceType = BuildReferenceType.DirectoryJunctions,
+            Manifest_Author = "AuthorTest",
+            Manifest_Donation = "http://test.com/test",
+            Manifest_AuthorEnabled = true,
+            Manifest_DonationEnabled = true
+        };
+
         [TestMethod]
-        public void Constructor_StillEquatable()
+        public void CopyConstructor_SettingsModel()
         {
             var expectedInstallPath = @"C:\Test\Testing";
             var GenUserWithTemplate = true;
@@ -37,6 +52,78 @@ namespace BSMT_Tests.Models
             var copy = new SettingsModel(original);
 
             Assert.AreEqual(original, copy);
+        }
+
+        [TestMethod]
+        public void CopyConstructor_ReadOnlySettingsModel()
+        {
+            var expectedInstallPath = @"C:\Test\Testing";
+            var GenUserWithTemplate = true;
+            var GenUserOnExisting = true;
+            var setManifestDefaults = true;
+            var copyOnBuild = true;
+            var expectedBRType = BuildReferenceType.DirectoryJunctions;
+            var manifestAuthor = "AuthorTest";
+            var manifestDonation = "http://test.com/test";
+            var authorEnabled = true;
+            var donationEnabled = true;
+            var original = new SettingsModel()
+            {
+                ChosenInstallPath = expectedInstallPath,
+                GenerateUserFileWithTemplate = GenUserWithTemplate,
+                GenerateUserFileOnExisting = GenUserOnExisting,
+                SetManifestJsonDefaults = setManifestDefaults,
+                CopyToIPAPendingOnBuild = copyOnBuild,
+                BuildReferenceType = expectedBRType,
+                Manifest_Author = manifestAuthor,
+                Manifest_Donation = manifestDonation,
+                Manifest_AuthorEnabled = authorEnabled,
+                Manifest_DonationEnabled = donationEnabled
+            };
+
+            var copy = new ReadOnlySettingsModel(original);
+
+            Assert.AreEqual(original, copy);
+        }
+
+        [TestMethod]
+        public void CopyConstructor_SettingsModel_IndividualTests()
+        {
+            var original = new SettingsModel();
+            var strType = typeof(string);
+            foreach (var prop in typeof(SettingsModel).GetProperties())
+            {
+                if (prop.PropertyType == typeof(string))
+                    prop.SetValue(original, prop.Name);
+                else if (prop.PropertyType == typeof(bool))
+                    prop.SetValue(original, !(bool)prop.GetValue(original));
+                else if (prop.PropertyType == typeof(BuildReferenceType))
+                    prop.SetValue(original, BuildReferenceType.DirectoryJunctions);
+                else
+                    Assert.Fail($"Type {prop.PropertyType} is unhandled for {prop.Name}");
+                var copy = new SettingsModel(original);
+                Assert.AreEqual(original, copy, $"Failed after setting {prop.Name}");
+            }
+        }
+
+        [TestMethod]
+        public void CopyConstructor_ReadOnlySettingsModel_IndividualTests()
+        {
+            var original = new SettingsModel();
+            var strType = typeof(string);
+            foreach (var prop in typeof(SettingsModel).GetProperties())
+            {
+                if (prop.PropertyType == typeof(string))
+                    prop.SetValue(original, prop.Name);
+                else if (prop.PropertyType == typeof(bool))
+                    prop.SetValue(original, !(bool)prop.GetValue(original));
+                else if (prop.PropertyType == typeof(BuildReferenceType))
+                    prop.SetValue(original, BuildReferenceType.DirectoryJunctions);
+                else
+                    Assert.Fail($"Type {prop.PropertyType} is unhandled for {prop.Name}");
+                var copy = new ReadOnlySettingsModel(original);
+                Assert.AreEqual(original, copy, $"Failed after setting {prop.Name}");
+            }
         }
     }
 }
