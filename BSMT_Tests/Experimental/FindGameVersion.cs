754ddfd9b0675865eaea7ebc34e4b8f22b5e3080
Zingabopp <Zingabopp@gmail.com>
2020-01-10 01:15:30 -0600

    .

--- BSMT_Tests/Experimental/FindGameVersion.cs
+++ BSMT_Tests/Experimental/FindGameVersion.cs
@@ -43,7 +43,7 @@ namespace BSMT_Tests.Experimental
         public void GameVersionTest()
         {
             string detectedVersion = GetVersion();
-            Assert.AreEqual("1.6.0", detectedVersion);
+            Assert.AreEqual("1.6.2", detectedVersion);
         }
     }
 }
