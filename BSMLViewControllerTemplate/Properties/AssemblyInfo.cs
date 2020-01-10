7ec2b4ce1ed3d0ff2a62f47781532eda0203d598
Zingabopp <Zingabopp@gmail.com>
2020-01-10 00:19:50 -0600

    Added BSMLViewController ItemTemplate

---
+++ BSMLViewControllerTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BSMLViewControllerTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jared Ball")]
+[assembly: AssemblyProduct("BSMLViewControllerTemplate")]
+[assembly: AssemblyCopyright("Copyright © Jared Ball 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b6e24d9-d390-46fb-8f72-50ab696ae846")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BSMLViewControllerTemplate/ViewControllerTemplate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$
+using BeatSaberMarkupLanguage;
+using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.ViewControllers;
+
+
+namespace $rootnamespace$
+{
+	internal class $safeitemrootname$ : BSMLResourceViewController
+	{
+		// For this method of setting the ResourceName, this class must be the first class in the file.
+		internal override string ResourceName => string.Join(".", GetType().Namespace, GetType().Name);
+	}
+}
