3991a8115f2be09a30b87b08d32ef041baaa2ab2
Zingabopp <Zingabopp@gmail.com>
2020-01-12 18:57:50 -0600

    Updates to Core template

--- BSIPA Plugin (Core)/Plugin.cs
+++ BSIPA Plugin (Core)/Plugin.cs
@@ -15,29 +15,39 @@ namespace $safeprojectname$
     [Plugin(RuntimeOptions.SingleStartInit)]
     public class Plugin
     {
+        internal static Plugin instance { get; private set; }
         internal static string Name => "$projectname$";
 
         [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
         public void Init(IPALogger logger)
         {
+            instance = this;
             Logger.log = logger;
             Logger.log.Debug("Logger initialized.");
         }
 
         #region BSIPA Config
         //Uncomment to use BSIPA's config
-        //[Init]
-        //public void InitWithConfig(Config conf)
-        //{
-        //    Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
-        //    Logger.log.Debug("Config loaded");
-        //}
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Logger.log.Debug("Config loaded");
+        }
+        */
         #endregion
 
         [OnStart]
         public void OnApplicationStart()
         {
             Logger.log.Debug("OnApplicationStart");
+            new GameObject("$safeprojectname$Controller").AddComponent<$safeprojectname$Controller>();
 
         }
 
