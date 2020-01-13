f4af5e025a6d5ab8ed20a563dd1b0fd2f996e77e
Zingabopp <Zingabopp@gmail.com>
2020-01-12 18:57:29 -0600

    .

--- BSIPA Plugin (Disableable)/PluginConfig.cs
+++ BSIPA Plugin (Disableable)/PluginConfig.cs
@@ -1,30 +1,32 @@
-﻿//using System.Runtime.CompilerServices;
-//using IPA.Config.Stores;
+﻿/*
+using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
 
-//[assembly: InternalsVisibleTo(GeneratedExtension.AssemblyVisibilityTarget)]
-//namespace $safeprojectname$.Configuration
-//{
-//    internal class PluginConfig
-//    {
-//        public static PluginConfig Instance { get; set; }
-//        public virtual int IntValue { get; set; } = 42;
+[assembly: InternalsVisibleTo(GeneratedExtension.AssemblyVisibilityTarget)]
+namespace $safeprojectname$.Configuration
+{
+    internal class PluginConfig
+    {
+        public static PluginConfig Instance { get; set; }
+        public virtual int IntValue { get; set; } = 42;
 
-//        public virtual void OnReload()
-//        {
-//            // Do stuff after config is read from disk.
-//        }
+        public virtual void OnReload()
+        {
+            // Do stuff after config is read from disk.
+        }
 
-//        /// <summary>
-//        /// Call this to force BSIPA to update the config file. This is also called by BSIPA if it detects the file was modified.
-//        /// </summary>
-//        public virtual void Changed()
-//        {
-//            // Do stuff when the config is changed.
-//        }
+        /// <summary>
+        /// Call this to force BSIPA to update the config file. This is also called by BSIPA if it detects the file was modified.
+        /// </summary>
+        public virtual void Changed()
+        {
+            // Do stuff when the config is changed.
+        }
 
-//        public virtual void CopyFrom(PluginConfig other)
-//        {
-//            // This instance's members populated from other
-//        }
-//    }
-//}
+        public virtual void CopyFrom(PluginConfig other)
+        {
+            // This instance's members populated from other
+        }
+    }
+}
+*/
