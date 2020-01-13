a08b809d9bd9ba29e37aef482e65f0f6a447f038
Zingabopp <Zingabopp@gmail.com>
2020-01-12 18:41:32 -0600

    Another Disableable update (final?)

--- BSIPA Plugin (Disableable)/Plugin.cs
+++ BSIPA Plugin (Disableable)/Plugin.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using IPA;
 using IPA.Config;
+using IPA.Config.Stores;
 using Harmony;
 using UnityEngine;
 using IPALogger = IPA.Logging.Logger;
@@ -18,11 +20,19 @@ namespace $safeprojectname$
         // public const string HarmonyId = "com.github.YourGitHub.$safeprojectname$";
         // internal static HarmonyInstance harmony => HarmonyInstance.Create(HarmonyId);
 
+        internal static Plugin instance { get; private set; }
         internal static string Name => "$projectname$";
+        internal static $safeprojectname$Controller PluginController => $safeprojectname$Controller.instance;
 
         [Init]
-        public void Init(IPALogger logger)
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public Plugin(IPALogger logger)
         {
+            instance = this;
             Logger.log = logger;
             Logger.log.Debug("Logger initialized.");
         }
@@ -46,23 +56,40 @@ namespace $safeprojectname$
         [OnEnable]
         public void OnEnable()
         {
+            new GameObject("$safeprojectname$Controller").AddComponent<$safeprojectname$Controller>();
             //ApplyHarmonyPatches();
         }
 
         /// <summary>
         /// Called when the plugin is disabled and on Beat Saber quit. It is important to clean up any Harmony patches, GameObjects, and Monobehaviours here.
         /// The game should be left in a state as if the plugin was never started.
+        /// Methods marked [OnDisable] must return void or Task.
         /// </summary>
         [OnDisable]
         public void OnDisable()
         {
+            if(PluginController != null)
+                GameObject.Destroy(PluginController);
             //RemoveHarmonyPatches();
         }
+
+     /*
+        /// <summary>
+        /// Called when the plugin is disabled and on Beat Saber quit.
+        /// Return Task for when the plugin needs to do some long-running, asynchronous work to disable.
+        /// [OnDisable] methods that return Task are called after all [OnDisable] methods that return void.
+        /// </summary>
+        [OnDisable]
+        public async Task OnDisableAsync()
+        {
+            await LongRunningUnloadTask().ConfigureAwait(false);
+        }
+    */
         #endregion
 
         // Uncomment the methods in this section if using Harmony
         #region Harmony
-    /*
+        /*
         /// <summary>
         /// Attempts to apply all the Harmony patches in this assembly.
         /// </summary>
