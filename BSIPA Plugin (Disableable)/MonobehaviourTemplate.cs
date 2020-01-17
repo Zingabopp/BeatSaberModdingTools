ddd9ecccc5154050c7851bcf06967508fff41b27
Zingabopp <Zingabopp@gmail.com>
2020-01-17 02:54:02 -0600

    Fixes to project template MonoBehaviour

--- BSIPA Plugin (Disableable)/MonobehaviourTemplate.cs
+++ BSIPA Plugin (Disableable)/MonobehaviourTemplate.cs
@@ -25,7 +25,11 @@ namespace $safeprojectname$
             // For this particular MonoBehaviour, we only want one instance to exist at any time, so store a reference to it in a static property
             //   and destroy any that are created while one already exists.
             if (instance != null)
+            {
+                Logger.log?.Warn($"Instance of {this.GetType().Name} already exists, destroying.");
                 GameObject.DestroyImmediate(this);
+                return;
+            }
             GameObject.DontDestroyOnLoad(this); // Don't destroy this object on scene changes
             instance = this;
             Logger.log?.Debug($"{name}: Awake()");
