e0247b14bcedd85210ee31439f9555ec8ba95dfb
Auros <aurosnex@gmail.com>
2020-04-20 23:25:04 -0400

    Added Lightweight Template

---
+++ BSIPA Plugin (Lightweight)/Logger.cs
@@ -0,0 +1,9 @@
+﻿using IPALogger = IPA.Logging.Logger;
+
+namespace $safeprojectname$
+{
+    internal static class Logger
+    {
+        internal static IPALogger log { get; set; }
+    }
+}
---
+++ BSIPA Plugin (Lightweight)/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace $safeprojectname$
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static string Name => "$projectname$";
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Instance = this;
+            Logger.log = logger;
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            
+        }
+    }
+}
